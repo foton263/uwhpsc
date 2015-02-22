37490971daa9e6ee8672c12771699d488264e39b
foton263 <foton263@gmail.com>
2015-02-21 21:40:52 -0800

    now test for convergence

--- mysqrt.py
+++ mysqrt.py
@@ -1,6 +1,12 @@
-x=2.
+x=10000.
 s=1.
-for k in range(6):
+kmax=100
+tol=1.e-10
+for k in range(kmax):
     print "Before iteration %s s equals %s" %(k,s)
+    s0=s
     s=0.5*(s+x/s)
+    delta_s=abs(s-s0)
+    if (delta_s/x) < tol:
+       break
 print s
